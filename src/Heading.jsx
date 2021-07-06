e4ca40829d05dff14bcbf4514390904704656939
CarstenHennig <carsten.hennig@gmail.com>
2021-07-06 13:03:30 +0200

    Just styling

---
+++ src/Heading.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function Heading() {
+    return (
+        <div className="heading">
+                <h1>My ultimative recipe of the day</h1>
+                <p>The daily menu special for you</p>
+            </div>
+        )
+}
+
+export default Heading;
